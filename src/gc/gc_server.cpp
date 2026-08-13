e53a6b6dcf526bf1b5acf08810c992415e905b5a
kerazz <nukedlcclient@gmail.com>
2026-08-13 20:19:44 +0300

    Update gc_server.cpp

--- src/gc/gc_server.cpp
+++ src/gc/gc_server.cpp
@@ -1,8 +1,4 @@
 #include "gc_server.h"
-#include "protobufs/base_gcmessages.pb.h"
-#include "protobufs/econ_gcmessages.pb.h"
-#include "protobufs/cstrike15_gcmessages.pb.h"
-#include "protobufs/gcsdk_gcmessages.pb.h"
 #include "inventory/manager.h"
 #include "store/store.h"
 #include <chrono>
@@ -41,15 +37,6 @@ void GCServer::Initialize(const std::string& config_path) {
     InventoryManager::Instance().Load(config_path);
     StoreManager::Instance().Init(config_path);
     
-    handlers_[4001] = std::bind(&GCServer::HandleHello, this, std::placeholders::_1);
-    handlers_[4002] = std::bind(&GCServer::HandleWelcome, this, std::placeholders::_1);
-    handlers_[9109] = std::bind(&GCServer::HandleClientHello, this, std::placeholders::_1);
-    handlers_[1001] = std::bind(&GCServer::HandleSetItemPosition, this, std::placeholders::_1);
-    handlers_[1004] = std::bind(&GCServer::HandleDeleteItem, this, std::placeholders::_1);
-    handlers_[1006] = std::bind(&GCServer::HandleNameItem, this, std::placeholders::_1);
-    handlers_[2534] = std::bind(&GCServer::HandleOpenCrate, this, std::placeholders::_1);
-    handlers_[2531] = std::bind(&GCServer::HandleAdjustEquipSlots, this, std::placeholders::_1);
-    
     Log("GCServer initialized for CS2");
 }
 
@@ -61,13 +48,7 @@ void GCServer::Shutdown() {
 }
 
 void GCServer::ProcessMessage(uint32_t msg_type, const void* data, uint32_t size) {
-    std::string raw_data(static_cast<const char*>(data), size);
-    auto it = handlers_.find(msg_type);
-    if (it != handlers_.end()) {
-        it->second(raw_data);
-    } else {
-        Log("Unhandled message type: %u", msg_type);
-    }
+    Log("Message received: type %u, size %u", msg_type, size);
 }
 
 void GCServer::SendToClient(uint32_t msg_type, const std::string& data) {
@@ -100,81 +81,6 @@ void GCServer::EquipItem(uint64_t item_id, bool equipped) {
             InventoryManager::Instance().UpdateItem(item);
             break;
         }
-    }
-}
-
-void GCServer::HandleHello(const std::string& data) {
-    CMsgClientHello hello;
-    if (!hello.ParseFromString(data)) {
-        Log("Failed to parse CMsgClientHello");
-        return;
-    }
-    
-    CMsgClientWelcome welcome;
-    welcome.set_version(1);
-    
-    std::string response;
-    welcome.SerializeToString(&response);
-    SendToClient(4002, response);
-    Log("Client hello processed, version: %u", hello.version());
-}
-
-void GCServer::HandleWelcome(const std::string& data) {
-    Log("Welcome message received");
-}
-
-void GCServer::HandleClientHello(const std::string& data) {
-    Log("CS2 ClientHello received");
-    
-    CMsgGCCStrike15_v2_GC2ClientHello response;
-    response.set_account_id(0);
-    
-    std::string resp_data;
-    response.SerializeToString(&resp_data);
-    SendToClient(9110, resp_data);
-}
-
-void GCServer::HandleSetItemPosition(const std::string& data) {
-    Log("SetItemPosition received");
-}
-
-void GCServer::HandleDeleteItem(const std::string& data) {
-    Log("DeleteItem received");
-}
-
-void GCServer::HandleNameItem(const std::string& data) {
-    Log("NameItem received");
-}
-
-void GCServer::HandleOpenCrate(const std::string& data) {
-    CMsgOpenCrate req;
-    if (!req.ParseFromString(data)) {
-        Log("Failed to parse CMsgOpenCrate");
-        return;
-    }
-    
-    Log("OpenCrate received, tool: %llu, subject: %llu", req.tool_item_id(), req.subject_item_id());
-    
-    GCItem new_item = StoreManager::Instance().OpenCrate(1001, 0);
-    if (new_item.def_index != 0) {
-        new_item.item_id = next_item_id_++;
-        AddItem(new_item);
-        Log("Crate opened, got item: %u", new_item.def_index);
-    }
-}
-
-void GCServer::HandleAdjustEquipSlots(const std::string& data) {
-    CMsgAdjustEquipSlots req;
-    if (!req.ParseFromString(data)) {
-        Log("Failed to parse CMsgAdjustEquipSlots");
-        return;
-    }
-    
-    Log("AdjustEquipSlots received, change_num: %u", req.change_num());
-    for (int i = 0; i < req.slots_size(); ++i) {
-        const CMsgAdjustEquipSlot& slot = req.slots(i);
-        Log("  Slot: class %u, slot %u, item %llu", slot.class_id(), slot.slot_id(), slot.item_id());
-        EquipItem(slot.item_id(), true);
     }
 }
 
